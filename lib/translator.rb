019221f73d204e19359c4a13e8d1ae0b96e6dc15
Andrew Matheny <androidmatheny@gmail.com>
2019-10-24 03:46:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ end
 
 def get_english_meaning(file,japanese_emoji)
   library = load_library(file)
-  if libary["get_meaning"].include?
+  if libary["get_meaning"].include?(japanese_emoji)
+    return 
   
 end
